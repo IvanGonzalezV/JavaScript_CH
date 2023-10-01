58fe741a8aebdc997a9113ef3b3542823d5ffe64
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2023-10-01 13:40:39 -0600

    repo_init :fire:

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("Curso de JS")
+
+// Clasificador de Dias Laborales
+
+let diaSemana = prompt("Ingrese un día de la semana:");
+
+if (diaSemana === "lunes" || diaSemana === "martes" || diaSemana === "miércoles" || diaSemana === "jueves" || diaSemana === "viernes") {
+  console.log("Día laboral");
+} else if (diaSemana === "sábado" || diaSemana === "domingo") {
+  console.log("Fin de semana");
+} else {
+  console.log("Día no válido");
+}
+
+
+// Clasificacion de Persona de acuerdo a la edad
+
+// let continuar = true;
+
+// while (continuar) {
+//   let edad = parseInt(prompt("Ingrese su edad:"));
+
+//   if (edad < 18) {
+//     alert("Eres un niño o un adolescente.");
+//   } else if (edad >= 18 && edad < 65) {
+//     alert("Eres un adulto.");
+//   } else {
+//     alert("Eres una persona mayor.");
+//   }
+
+//   const respuesta = prompt("Desea continuar? Ingrese 'esc' para salir, o cualquier otra tecla para continuar.");
+
+//   if (respuesta !== null && respuesta.toLowerCase() === 'esc') {
+//     continuar = false; 
+//   }
+// }
+
+
+
+// Tabla de Multiplicar opciones de rango y tabla
+
+// let numero = parseInt(prompt("Ingrese un número para ver su tabla de multiplicar:"));
+// let limiteSuperior = parseInt(prompt("Ingrese el limite superior del rango:"));
+
+// for (let i = 1; i <= limiteSuperior; i++) {
+//   let resultado = numero * i;
+//   console.log(`${numero} x ${i} = ${resultado}`);
+//   /* alert(`${numero} x ${i} = ${resultado}`); */
+// }
+
+
+// Aplicacion de IVA
+
+/* const iva = 1.16
+let monto1 = parseInt(prompt("Ingrese el primer monto"))
+let monto2 = parseInt(prompt("Ingrese el siguiente monto"))
+let monto3 = parseInt(prompt("Ingrese el siguiente monto"))
+let monto4 = parseInt(prompt("Ingrese el siguiente monto"))
+let monto5 = parseInt(prompt("Ingrese el siguiente monto"))
+const totalSinIva = monto1 + monto2 + monto3 + monto4 + monto5
+const totalConIva = Math.round(totalSinIva * iva)
+
+alert("Monto sin IVA: " + "$" + totalSinIva + "\nMonto con IVA: " + "$" + totalConIva) */
