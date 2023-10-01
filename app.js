2ab3be025a01fd449649423f893508ca5e0c03dc
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2023-10-01 14:21:02 -0600

    last:zap:

--- app.js
+++ app.js
@@ -1,52 +1,39 @@
 console.log("Curso de JS")
 
-// Clasificador de Dias Laborales
+// EJERCICIO 1: Clasificacion de Persona de acuerdo a la edad
 
-let diaSemana = prompt("Ingrese un día de la semana:");
+/* let continuar = true;
 
-if (diaSemana === "lunes" || diaSemana === "martes" || diaSemana === "miércoles" || diaSemana === "jueves" || diaSemana === "viernes") {
-  console.log("Día laboral");
-} else if (diaSemana === "sábado" || diaSemana === "domingo") {
-  console.log("Fin de semana");
-} else {
-  console.log("Día no válido");
+while (continuar) {
+  let edad = parseInt(prompt("Ingrese su edad:"));
+
+  if (edad < 18) {
+    alert("Eres un niño o un adolescente.");
+  } else if (edad >= 18 && edad < 65) {
+    alert("Eres un adulto.");
+  } else {
+    alert("Eres una persona mayor.");
+  }
+
+  const respuesta = prompt("Desea continuar? Ingrese 'esc' para salir, o cualquier otra tecla para continuar.");
+
+  if (respuesta !== null && respuesta.toLowerCase() === 'esc') {
+    continuar = false; 
+  }
+} */
+
+
+
+// EJERCICIO 2: Tabla de Multiplicar con opciones de rango y tabla
+
+let numero = parseInt(prompt("Ingrese un número para ver su tabla de multiplicar:"));
+let limiteSuperior = parseInt(prompt("Ingrese el limite superior del rango:"));
+
+for (let i = 1; i <= limiteSuperior; i++) {
+  let resultado = numero * i;
+  console.log(`${numero} x ${i} = ${resultado}`);
+//   alert(`${numero} x ${i} = ${resultado}`);
 }
-
-
-// Clasificacion de Persona de acuerdo a la edad
-
-// let continuar = true;
-
-// while (continuar) {
-//   let edad = parseInt(prompt("Ingrese su edad:"));
-
-//   if (edad < 18) {
-//     alert("Eres un niño o un adolescente.");
-//   } else if (edad >= 18 && edad < 65) {
-//     alert("Eres un adulto.");
-//   } else {
-//     alert("Eres una persona mayor.");
-//   }
-
-//   const respuesta = prompt("Desea continuar? Ingrese 'esc' para salir, o cualquier otra tecla para continuar.");
-
-//   if (respuesta !== null && respuesta.toLowerCase() === 'esc') {
-//     continuar = false; 
-//   }
-// }
-
-
-
-// Tabla de Multiplicar opciones de rango y tabla
-
-// let numero = parseInt(prompt("Ingrese un número para ver su tabla de multiplicar:"));
-// let limiteSuperior = parseInt(prompt("Ingrese el limite superior del rango:"));
-
-// for (let i = 1; i <= limiteSuperior; i++) {
-//   let resultado = numero * i;
-//   console.log(`${numero} x ${i} = ${resultado}`);
-//   /* alert(`${numero} x ${i} = ${resultado}`); */
-// }
 
 
 // Aplicacion de IVA
